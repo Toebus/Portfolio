5048f059a171e853e13feb7af8ab3250e8a64ebd
Tobias <t-guld@hotmail.com>
2024-03-11 11:16:19 +0100

    Shapecalc -- Denne kunne man god forbedre meget mere =)

---
+++ ShapeCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string answer;
+        float result = 0;
+        Console.WriteLine("What shape would you like to find the area of? please enter 'r' for Rectangle or 'c' for circle");
+
+        answer = Console.ReadLine();
+
+        if (answer == "r") 
+        {
+            Console.WriteLine("Please enter the height of the Rectangle ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please ennter the width of the Rectangle");
+            float width = float.Parse(Console.ReadLine());
+            result = height * width;
+
+        }
+        else if (answer == "c")
+        {
+            Console.WriteLine("Please enter the radius of the circle");
+            float radius = float.Parse(Console.ReadLine());
+            result = (float)Math.PI * (radius * radius);
+
+        }
+        else { Console.WriteLine("Please enter 'r' for Rectangle or 'c' for circle");
+        }
+
+        Console.WriteLine("The result is: " + result);
+
+        Console.ReadKey();
+    }
+}
